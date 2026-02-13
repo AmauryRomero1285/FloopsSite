784b0d2da66e5039143ba7ebdc4b220b1032f318
AmauryRomero1285 <230190@utxicotepec.edu.mx>
2026-02-13 17:30:21 -0600

    First Commit
    Add th directories structure for e-commerce

---
+++ create.bat
@@ -0,0 +1,14 @@
+@echo off
+mkdir "public" 2>nul
+mkdir "src\app\frutas-y-verduras" 2>nul
+mkdir "src\app\carnes-y-pescados" 2>nul
+mkdir "src\app\lacteos-y-huevos" 2>nul
+mkdir "src\app\panaderia-y-reposteria" 2>nul
+mkdir "src\app\bebidas" 2>nul
+mkdir "src\app\limpieza-y-hogar" 2>nul
+mkdir "src\app\cuidado-personal" 2>nul
+mkdir "src\app\dental" 2>nul
+mkdir "src\app\carrito" 2>nul
+mkdir "src\app\checkout" 2>nul
+echo Estructura generada correctamente.
+pause
